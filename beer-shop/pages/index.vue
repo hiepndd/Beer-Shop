c412edd372717727d68485cfec6ae56ed989b1dd
hiepndd <nguyendangdaihiep@gmail.com>
2018-08-14 12:03:32 +0700

    Modify index

--- beer-shop/pages/index.vue
+++ beer-shop/pages/index.vue
@@ -1,39 +1,63 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        beer-shop
-      </h1>
-      <h2 class="subtitle">
-        My primo Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
+  <div class="container mx-auto max-w-full">
+   <div class="flex">
+    <div class="container w-1/3 list" >
+      <ListBeer/>
+
     </div>
-  </section>
+    <div class="container mx-auto w-2/3 mt-20 listdetail">
+           <div class="ml-10">
+            <h1><nuxt-child :key="$route.params.id"/></h1>
+           </div>
+    </div>
+   </div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
 import Logo from '~/components/Logo.vue'
+import ListBeer from '~/components/ListBeer.vue'
+import BeerDetail from '~/components/BeerDetail.vue'
 
 export default {
+
   components: {
-    Logo
+    Logo,
+    ListBeer,
+    BeerDetail,
+  },
+
+  data() {
+    return {
+      listBeer: [],
+      textSearch: '',
+      select: '',
+    }
+  },
+
+  methods: {
+  search(){
+       axios.get('https://api.punkapi.com/v2/beers?beer_name=' + this.textSearch).then(response => {
+         this.listBeer = response.data
+         if (this.listBeer.length === 0){document.getElementById('error').innerHTML = 'No Results Found'}
+         else {document.getElementById('error').innerHTML = ' '}
+       })
+  },
+
+
   }
+
+
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Bitter|Josefin+Sans');
+@import url('https://fonts.googleapis.com/css?family=Lekton');
 .container
 {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+
 }
 .title
 {
@@ -56,4 +80,31 @@ export default {
 {
   padding-top: 15px;
 }
+#search {
+   width: 100%;
+  font-size: 16px;
+   padding: 12px 20px 12px 40px;
+   border: 1px solid #ddd;
+   border-radius: 5px;
+}
+#listBeer {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  color: black;
+  display: block;
+  font-size: 18px;
+  padding: 12%;
+}
+.listdetail {
+  font-family: 'Bitter', serif;
+}
+.list {
+  font-family: 'Lekton', sans-serif;
+}
+li:hover{
+  border: 2px dotted black;
+}
+
+
 </style>
